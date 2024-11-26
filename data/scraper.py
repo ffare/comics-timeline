f632fe756f8116e1930dac7fc4ea76768fd55944
ffare <faresbenlakdher@gmail.com>
2024-11-26 19:12:27 +0100

    more work on the scraper - fetching data from issues

--- data/scraper.py
+++ data/scraper.py
@@ -16,4 +16,17 @@ soup = fetch_html(base_url)
 for gallery_item in soup.find_all('div', class_='wikia-gallery-item'):    
     issue_link = gallery_item.find('div', class_='lightbox-caption').a.get('href')  # Get links for issues
     
-    print(issue_link)
+    issue_soup = fetch_html(f"https://marvel.fandom.com{issue_link}")
+    
+    issue_cover = issue_soup.find('a', class_='image image-thumbnail').img['src']
+    
+    issue_data = issue_soup.find('section', class_='pi-item pi-group pi-border-color pi-collapse pi-collapse-open')
+    #print(issue_data.prettify())
+    
+    writer = issue_data.find('div', {'data-source': 'Writers1'}).a.text
+    #issue_number
+    
+    print(issue_cover)
+    time.sleep(2)
+    
+    
